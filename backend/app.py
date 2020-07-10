831ff0967b4ddc936e2c63cbf13e38dea548f867
Philip Zhang <jmslca123@gmail.com>
2020-07-10 12:19:07 -0700

    implement models and login/registration routes

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 import random
-
+import datetime
 from config import DEBUG
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from models import db
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    current_user,
+    login_required,
+)
+from models import db, User, Auction, Bid
 
 app = Flask(__name__)
 CORS(app)
@@ -16,12 +23,110 @@ app.config["SECRET_KEY"] = "".join([chr(random.randint(65, 92)) for _ in range(5
 db.app = app
 db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.is_json:
+        return jsonify({"message": "Missing JSON in request"}), 400
+
+    username = request.json.get("username", None)
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    confirm = request.json.get("confirm", None)
+    if not username:
+        return jsonify({"message": "Username is required."}), 400
+    if not email:
+        return jsonify({"message": "Email is required."}), 400
+    if not password:
+        return jsonify({"message": "Password is required."}), 400
+    if not confirm:
+        return jsonify({"message": "Password confirmation is required."}), 400
+    if password != confirm:
+        return jsonify({"message": "Passwords do not match"}), 400
+
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return jsonify({"message": "User already exists."}), 400
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return jsonify({"message": "Email already exists."}), 400
+
+    user = User(username=username, email=email, created=datetime.datetime.utcnow(),)
+    user.set_password(password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    login_user(user)
+    return jsonify(message="Successfully registered!", user=user.to_dict()), 200
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    if not request.is_json:
+        return jsonify({"message": "Missing JSON in request"}), 400
+
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if not username:
+        return jsonify({"message": "Username is required."}), 400
+    if not password:
+        return jsonify({"message": "Password is required."}), 400
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        return jsonify({"message": "User not found."}), 400
+
+    if not user.check_password(password):
+        return jsonify({"message": "Invalid password."}), 400
+
+    login_user(user)
+    return jsonify(message="Successfully logged in!", user=user.to_dict()), 200
+
+
+@app.route("/logout", methods=["DELETE"])
+def logout():
+    """Log user out"""
+    logout_user()
+    return jsonify(message="Successfully logged out"), 200
+
+
+@app.route("/", methods=["GET"])
+@login_required
+def home():
+    return jsonify(current_user.to_dict())
+
 
 if __name__ == "__main__":
-    # if DEBUG:
-    # db.drop_all()
-    # db.create_all()
+    if DEBUG:
+        db.drop_all()
+        db.create_all()
 
-    # db.session.add_all()
-    # db.session.commit()
+        u1 = User(
+            username="test", email="test@gmail.com", created=datetime.datetime.utcnow(),
+        )
+        u1.set_password("test")
+        a1 = Auction(
+            name="test auction",
+            description="test desc",
+            starting_price=150.05,
+            created=datetime.datetime.utcnow(),
+            end_time=datetime.datetime(2020, 8, 8, 7, 43),
+        )
+        b1 = Bid(amount=100.01, created=datetime.datetime.utcnow())
+        a1.bids.append(b1)
+        u1.auctions.append(a1)
+        u1.bids.append(b1)
+        db.session.add(u1)
+        db.session.commit()
     app.run(debug=DEBUG)
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,85 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
 
 db = SQLAlchemy()
+
+
+class User(UserMixin, db.Model):
+    """
+    User Model
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+    auctions = db.relationship("Auction", backref="user")
+    bids = db.relationship("Bid", backref="user")
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def to_dict(self):
+        """returns dict representation of User"""
+        return {
+            "id": self.id,
+            "username": self.username,
+            "created": self.created,
+            "auctions": [auction.to_dict() for auction in self.auctions],
+            "bids": [bid.to_dict() for bid in self.bids],
+        }
+
+
+class Auction(db.Model):
+    """
+    Auction Model
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    starting_price = db.Column(db.Float, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+    bids = db.relationship("Bid", backref="auction")
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def to_dict(self):
+        """returns dict representation of Auction"""
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "starting_price": self.starting_price,
+            "created": self.created,
+            "end_time": self.end_time,
+            "bids": [bid.to_dict() for bid in self.bids],
+            "user_id": self.user_id,
+        }
+
+
+class Bid(db.Model):
+    """
+    Bid Model
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    auction_id = db.Column(db.Integer, db.ForeignKey("auction.id"), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+
+    def to_dict(self):
+        """returns dict representation of Bid"""
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "auction_id": self.auction_id,
+            "amount": self.amount,
+            "created": self.created,
+        }
