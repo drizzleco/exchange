8cefaae14d81287f1ef13838c47f1f3ae4795b5d
Philip Zhang <jmslca123@gmail.com>
2020-07-17 14:54:12 -0700

    move test data creation to tests.py

---
+++ backend/tests.py
@@ -0,0 +1,22 @@
+from models import Auction, Bid, User, db
+import datetime
+
+db.drop_all()
+db.create_all()
+
+u1 = User(username="test", email="test@gmail.com", created=datetime.datetime.utcnow(),)
+u1.set_password("test")
+a1 = Auction(
+    name="test auction",
+    description="test desc",
+    starting_price=99.99,
+    created=datetime.datetime.utcnow(),
+    end_time=datetime.datetime.utcnow() + datetime.timedelta(days=7),
+    user=u1,
+)
+b1 = Bid(
+    amount=100.01, created=datetime.datetime(2020, 7, 13, 7, 43), user=u1, auction=a1,
+)
+b2 = Bid(amount=220, created=datetime.datetime.utcnow(), user=u1, auction=a1,)
+db.session.add(u1)
+db.session.commit()
