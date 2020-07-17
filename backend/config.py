846d112d5aee4e67e0618d2454c371c7b8f8b0e0
Philip Zhang <jmslca123@gmail.com>
2020-07-17 14:48:57 -0700

    update config.py to use PORT var

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,13 @@
-import random
+import random, os
 
 DEBUG = True
-ORIGINS = ["http://localhost:8080", "http://127.0.0.1:8080"]
+PORT = os.getenv("PORT") or 8080
+ORIGINS = ["http://localhost:" + str(PORT), "http://127.0.0.1:" + str(PORT)]
 
 
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///exchange.sqlite"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "".join([chr(random.randint(65, 92)) for _ in range(50)])
+    SECRET_KEY = os.getenv("SECRET_KEY") or "".join(
+        [chr(random.randint(65, 92)) for _ in range(50)]
+    )
